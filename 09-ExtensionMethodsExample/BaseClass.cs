3b05e6b8386d20c36d819c76cfa9fb46371607dd
EliasZare <1EliasZare1@gmail.com>
2024-10-23 12:25:20 +0330

    Add project files.

---
+++ 09-ExtensionMethodsExample/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_ExtensionMethodsExample
+{
+    public  class BaseClass
+    {
+
+       // public int non_Nullable = null;
+        public int? Nullable = null;
+       public dynamic Name = "";
+    }
+}
---
+++ 09-ExtensionMethodsExample/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _09_ExtensionMethodsExample;
+
+Console.WriteLine("Hello, World!");
+BaseClass baseClass = new BaseClass();
+baseClass.GetYear(1110);
+
+baseClass.Nullable = null;
+baseClass.Name = "Elias";
+Console.WriteLine(baseClass.Name);
+baseClass.Name = 110;
+Console.WriteLine(baseClass.Name);
+if (baseClass.Nullable is null)
+{
+    Console.WriteLine("is Null");
+}
